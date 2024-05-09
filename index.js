298d325855b2713d15ce2947233ea39faf729c61
ftweyi <gs1marcasds@hotmail.com>
2024-05-09 20:11:54 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function buildTree(preorder, inorder) {
-  if (preorder.length === 0 || inorder.length === 0) return null;
-  const rootVal = preorder[0];
-  const root = new TreeNode(rootVal);
-  const index = inorder.indexOf(rootVal);
-  const leftInorder = inorder.slice(0, index);
-  const rightInorder = inorder.slice(index + 1);
-  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
-  const rightPreorder = preorder.slice(1 + leftInorder.length);
-  root.left = buildTree(leftPreorder, leftInorder);
-  root.right = buildTree(rightPreorder, rightInorder);
-  return root;
-}
+const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
