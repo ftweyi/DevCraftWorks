938c57d09e90c57b6c464c2759966843f9f6a32f
ftweyi <gs1marcasds@hotmail.com>
2024-05-10 12:47:40 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
