6054c5c6ff7c2d2c36e60e3234313e81330fcf92
ftweyi <gs1marcasds@hotmail.com>
2024-05-14 02:27:50 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-function calculate(s) {
-  const stack = [];
-  let num = 0;
-  let sign = "+";
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    if (!isNaN(parseInt(char)) && char !== " ") {
-      num = num * 10 + parseInt(char);
-    }
-    if (isNaN(parseInt(char)) || i === s.length - 1) {
-      if (sign === "+") stack.push(num);
-      else if (sign === "-") stack.push(-num);
-      else if (sign === "*") stack.push(stack.pop() * num);
-      else if (sign === "/") stack.push(parseInt(stack.pop() / num));
-      num = 0;
-      sign = char;
-    }
-  }
-  return stack.reduce((acc, val) => acc + val, 0);
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
