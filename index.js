ab9596a6319fe5b0e16cf017d816b974c18fd3cb
ftweyi <gs1marcasds@hotmail.com>
2024-05-15 04:56:59 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const intersection = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => s.has(x));
-};
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
+}
