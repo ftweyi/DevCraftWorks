c443b17c7c6b1d1053b2df68269c7ddbda06854d
ftweyi <gs1marcasds@hotmail.com>
2024-05-16 09:56:13 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-function longestIncreasingPath(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows), () => Array(cols).fill(0));
-  let longestPath = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
+function deserialize(data) {
+  if (!data.length) return null;
+  const root = new TreeNode(data.shift());
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    const leftVal = data.shift();
+    if (leftVal !== undefined) {
+      node.left = new TreeNode(leftVal);
+      queue.push(node.left);
+    }
+    const rightVal = data.shift();
+    if (rightVal !== undefined) {
+      node.right = new TreeNode(rightVal);
+      queue.push(node.right);
     }
   }
-  return longestPath;
-  function dfs(matrix, i, j, dp) {
-    if (dp[i][j] !== 0) return dp[i][j];
-    const dirs = [
-      [0, 1],
-      [0, -1],
-      [1, 0],
-      [-1, 0],
-    ];
-    let maxPath = 1;
-    for (const dir of dirs) {
-      const newRow = i + dir[0];
-      const newCol = j + dir[1];
-      if (
-        newRow >= 0 &&
-        newRow < matrix.length &&
-        newCol >= 0 &&
-        newCol < matrix[0].length &&
-        matrix[newRow][newCol] > matrix[i][j]
-      ) {
-        maxPath = Math.max(maxPath, 1 + dfs(matrix, newRow, newCol, dp));
-      }
-    }
-    dp[i][j] = maxPath;
-    return maxPath;
-  }
+  return root;
 }
