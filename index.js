3994aacec5dc2dc5ac0ad5b6c69a77f386e8229b
ftweyi <gs1marcasds@hotmail.com>
2024-05-17 10:41:13 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-function deserialize(data) {
-  if (!data.length) return null;
-  const root = new TreeNode(data.shift());
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    const leftVal = data.shift();
-    if (leftVal !== undefined) {
-      node.left = new TreeNode(leftVal);
-      queue.push(node.left);
-    }
-    const rightVal = data.shift();
-    if (rightVal !== undefined) {
-      node.right = new TreeNode(rightVal);
-      queue.push(node.right);
-    }
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  return root;
-}
+  return gcd(b, a % b);
+};
